42b6b62cd7b44a1057e6c499a8a11b66b4495068
arjunpanesar <114442546+arjunpanesar@users.noreply.github.com>
2025-03-13 15:24:20 +0000

    keypad functionality(still need to test)

--- keypad.h
+++ keypad.h
@@ -1,54 +1,73 @@
 #pragma once
+
 #include "mbed.h"
-#include "keypad.h"
 
-class Keypad
-{
-    public:
+// Value to indicate no key pressed
+#define NO_KEY 0
 
-    // constructor for the keypad class - you need to supply the pin names to which the rows/
-    // and columns are connected
-    Keypad(PinName row0, 
-           PinName row1, 
-           PinName row2, 
-           PinName row3, 
-           PinName col0, 
-           PinName col1, 
+class Keypad {
+public:
+    /**
+     * Constructor for the Keypad class.
+     * The pin assignments are provided as parameters—update these 
+     * according to the wiring on your MKL46Z256 board along with the datasheets:
+     *   e.g. row pins could be PTA14, PTA15, PTA16, PTA17 and
+     *        column pins could be PTA5, PTA6, PTA7 (or update as needed).
+     */
+    Keypad(PinName row0,
+           PinName row1,
+           PinName row2,
+           PinName row3,
+           PinName col0,
+           PinName col1,
            PinName col2);
 
-    // function that reads a key - returns mmediately with the key pressed or NO_KEY
-    // only one instance of a key value will be returned - coincident with the onset of 
-    // the key being pressed. Only publicly available member function
+    /**
+     * ReadKey() checks the latest key scan.
+     * It returns a key value ONLY at the moment that
+     * the key has changed since the last request.
+     * Otherwise, it returns NO_KEY.
+     */
     char ReadKey(void);
 
-   // value that means no key is pressed
-   #define NO_KEY 0
-
-    protected:
-
-    private:
-
-    // this key scan function does most of the heavy lifting it is intended to be spawned as 
-    // an independent thread that runs in the background
+protected:
+private:
+    /**
+     * KeyScanner() is launched as a background thread.
+     * It sequentially scans each of the three keypad columns by
+     * setting it low and reading the four rows.
+     *
+     * Mapping:
+     *   Column 0: { '1', '4', '7', '#' }
+     *   Column 1: { '2', '5', '8', '0' }
+     *   Column 2: { '3', '6', '9', '*' }
+     */
     void KeyScanner(void);
 
-    // thread to run the key scanner every 10ms
+    // Thread that continuously scans the keys
     Thread keyscan;
 
-    // class definitions for the rwos (inputs) and columns (outputs)
-    // however these objects are not fully initialised (i.e. the In/Outs
-    // have not been associated with pins. This is done when the class is
-    // initialised - see the associations after the colon in ketypad::keypad )
-    DigitalIn _row0, _row1, _row2, _row3;
-    DigitalOut _col0, _col1, _col2;
+    // The row input pins (digital reads)
+    DigitalIn _row0;
+    DigitalIn _row1;
+    DigitalIn _row2;
+    DigitalIn _row3;
 
-    // keypad mapping key position to value
-    // this returns printable ascii codes corresponding to each key
-    char mapping[3][4] = {{'1','4','7','#'},
-                          {'2','5','8','0'},
-                          {'3','6','9','*'}};
+    // The column output pins (digital writes)
+    DigitalOut _col0;
+    DigitalOut _col1;
+    DigitalOut _col2;
 
-    // variables shared between keyscanner and read key
+    // Key mapping for a 3-column x 4-row keypad.
+    char mapping[3][4] = {
+        {'1', '4', '7', '#'},
+        {'2', '5', '8', '0'},
+        {'3', '6', '9', '*'}
+    };
+
+    // Shared variables between the KeyScanner thread and ReadKey.
+    // 'key' holds the instantaneous key value,
+    // 'key_p' tracks the previous key value reported.
     char key, key_p;
-
 };
+
--- main.cpp
+++ main.cpp
@@ -6,21 +6,26 @@
 #include "mbed.h"
 #include "keypad.h"
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
-
-int main()
-{
-    // Initialise the digital pin LED1 as an output
-//#ifdef LED1
+// Create an LED output (LED1) and a Keypad instance.
+// These pin assignments match those in your keypad module.
+// Update the pin names to match your hardware if necessary.
 DigitalOut led(LED1);
-//#else
-//    bool led;
-//#endif
+Keypad keypad(PTA14, PTA15, PTA16, PTA17, PTA5, PTA6, PTA7);
 
+int main() {
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        // Read the debounced key from the keypad.
+        char key = keypad.ReadKey();
+        
+        // Turn on the LED if any key is pressed (i.e. key is not NO_KEY).
+        if(key != NO_KEY) {
+            led = 1;
+        } else {
+            led = 0;
+        }
+        
+        // Short delay so the loop doesn't hog the processor.
+        ThisThread::sleep_for(10ms);
     }
 }
+
