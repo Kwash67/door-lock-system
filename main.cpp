124ace76eb2dc88e274c2503a128527774f49332
Kwashie A <kwashieee@gmail.com>
2025-03-18 18:40:08 +0000

    keypad works

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
- // #include "display.h"
- //int main()
- //display()
-
+#undef __ARM_FP // removes the error on the #include "mbed.h" line
 #include "mbed.h"
 #include "keypad.h"
 #include "SLCD.h"
@@ -14,52 +11,43 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-SLCD md;
-
-int s = 0;
-int m = 0;
-
-//System Status LED
-DigitalOut led(LED1);
-
-//Instantiation of the keypad
-Keypad Keypad(PTA12, PTA4, PTA5, PTC8, PTD3, PTA2, PTA1);
-
-/*(        PinName PTA12 row0,
-           PinName PTA4 row1,
-           PinName PTA5 row2,
-           PinName PTC8 row3,
-           PinName PTD3 col0,
-           PinName PTA2 col1,
-           PinName PTA1 col2);
-*/
-
-//Instantiation of the display
-Display disp; //This is an example for now as display.h and display.cpp does not exist
-
 
 int main() {
+    SLCD md;
+
+    int s = 0;
+    int m = 0;
+
+    // System Status LED
+    DigitalOut led(LED1);
+    DigitalOut led2(LED2);
+
+    //Instantiation of the keypad
+    Keypad Keypad(PTC8, PTA5, PTA4, PTA12, PTD3, PTA2, PTA1);
+    // Keypad Keypad(PTA12, PTA4, PTA5, PTC8, PTD3, PTA2, PTA1);
+
     while (true) {
         led = !led;
-        md.printf("%02d%02d", m, s);
-        md.Colon(1);
-        ThisThread::sleep_for(BLINKING_RATE);
-        md.Colon(0);
-        ThisThread::sleep_for(BLINKING_RATE);
-        s++;
-        if (s>=60) {
-            s = 0;
-            m++;
-        }
+        // md.printf("%02d%02d", m, s);
+        // md.Colon(1);
+        // ThisThread::sleep_for(BLINKING_RATE);
+        // md.Colon(0);
+        // ThisThread::sleep_for(BLINKING_RATE);
+        // s++;
+        // if (s>=60) {
+        //     s = 0;
+        //     m++;
+        // }
 
         // Read the debounced key from the keypad.
         char key = Keypad.ReadKey();
         
         // Turn on the LED if any key is pressed (i.e. key is not NO_KEY).
         if(key != NO_KEY) {
-            led = 1;  //Access control probably needs to be implemented here 
+            led2 = 1;  //Access control probably needs to be implemented here 
+            md.printf("%c",key);
         } else {
-            led = 0;
+            led2 = 0;
         }
         
         // Short delay so the loop doesn't hog the processor.
